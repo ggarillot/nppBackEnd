6ccc2761cc7d05456978e2eb355b5df3dda174be
Guillaume GARILLOT <gari.guillaume@gmail.com>
2019-10-21 14:15:05 +0200

    first commit

---
+++ src/main/java/com/npp/NppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.npp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NppBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NppBackEndApplication.class, args);
+	}
+
+}
