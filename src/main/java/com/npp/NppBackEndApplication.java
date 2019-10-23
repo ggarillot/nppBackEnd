e359dfb2f2f3c81f848490924b15771c60b559a4
sarahnicoud <nicoud.sarah.38@gmail.com>
2019-10-23 12:25:24 +0200

    add of th TestUnit

--- src/main/java/com/npp/NppBackEndApplication.java
+++ src/main/java/com/npp/NppBackEndApplication.java
@@ -1,31 +1,13 @@
 package com.npp;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.npp.entity.Administrator;
-import com.npp.iservice.IGenericUserService;
-
 @SpringBootApplication
-public class NppBackEndApplication implements CommandLineRunner {
-
-	@Autowired
-	IGenericUserService userService;
+public class NppBackEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NppBackEndApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		Administrator a = new Administrator();
-		a.setEmail("toto@titi.com");
-		a.setName("titi");
-
-		userService.add(a);
 	}
 
 }
--- src/main/java/com/npp/iservice/IAdministratorService.java
+++ src/main/java/com/npp/iservice/IAdministratorService.java
@@ -1,7 +1,9 @@
 package com.npp.iservice;
 
+import java.util.List;
+
 import com.npp.entity.Administrator;
 
 public interface IAdministratorService extends IAbstractService<Administrator, Long> {
-
+	List<Administrator> findByName(String name);
 }
